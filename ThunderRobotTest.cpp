961ea5b6bf716490f9539b188fcd5b77e5e03352
Wilmer Gaona <wgaonar@gmail.com>
2022-03-08 05:52:47 -0600

    Duration values were corrected

--- ThunderRobotTest.cpp
+++ ThunderRobotTest.cpp
@@ -23,6 +23,12 @@ int main()
   cout << "Distance to ground: " << distanceToGround << "cm\n";
   DelayMilliseconds(250);
 
+  /*
+  cout << "\nMeasuring the distance to front\n";
+  double distanceToFront= myRobot.GetDistanceToFront();
+  cout << "Distance to front: " << distanceToFront << "cm\n";
+  DelayMilliseconds(250);
+
   cout << "\nMeasuring the distance to right\n";
   double distanceToRight= myRobot.GetDistanceToRight();
   cout << "Distance to right: " << distanceToRight << "cm\n";
@@ -32,24 +38,45 @@ int main()
   double distanceToLeft= myRobot.GetDistanceToLeft();
   cout << "Distance to left: " << distanceToLeft << "cm\n";
   DelayMilliseconds(250);
-  
-  cout << "\nMeasuring the distance to front\n";
-  double distanceToFront= myRobot.GetDistanceToFront();
-  cout << "Distance to front: " << distanceToFront << "cm\n";
-  DelayMilliseconds(250);
+  */
 
   // Test the robot movements
   cout << "\nMoving the ThunderRobot...\n";
 
   int motorSpeed {100};
+  /*
   myRobot.Move("forward", motorSpeed, 1000);
   myRobot.Move("backward", motorSpeed, 1000);
   
   myRobot.Turn("left", motorSpeed, 1000);
   myRobot.Turn("right", motorSpeed, 1000);
+  */
+
+  /*
+  for (size_t i = 0; i < 4; i++)
+  {
+    myRobot.Move10cm("forward");
+    myRobot.Move10cm("backward");
+  }
+  */
+
 
   myRobot.Turn90Degrees("left");
+  DelayMilliseconds (1000);
+
+  myRobot.Turn45Degrees("left");
+  DelayMilliseconds (1000);
+  
+  myRobot.Turn30Degrees("left");
+  DelayMilliseconds (1000);
+
+  /*
   myRobot.Turn90Degrees("right");
+
+  myRobot.Turn45Degrees("right");
+
+  myRobot.Turn30Degrees("right");
+  */
 
   message = "Main program finishes here...";
   cout << RainbowText(message,"Blue", "White","Bold") << endl;
